38b532e4c7471da00584e9c46af67d36f3b2dce4
Daniel Woelfel <dww@fb.com>
2016-10-27 02:03:54 +0200

    example

--- src/App.js
+++ src/App.js
@@ -1,20 +1,147 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Map, List } from 'immutable';
+import { projectState, addListener, updateState } from './State.js';
 
-class App extends Component {
+class ServerRequest extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div style={{marginBottom: 20}}>
+        Add comment:
+        <pre>
+          {JSON.stringify(this.props.comment.delete('commentsBefore').toJS(), null, '\t')}
+        </pre>
+        <button onClick={this._onSuccess}>Succeed</button>
+        {' '}
+        <button onClick={this._onFail}>Fail</button>
       </div>
     );
+  }
+
+  _onFail = () => {
+    const comment = this.props.comment;
+    updateState(state => state.set('comments', comment.get('commentsBefore') || List()));
+  }
+
+  _onSuccess = () => {
+    const comment = this.props.comment;
+    updateState((state) => {
+      return state.update('comments', (comments) => {
+        return comments.map(c => {
+          if (c === comment) {
+            return c.set('optimistic', false);
+          }
+          return c;
+        })
+      })
+    });
+  }
+}
+
+class Comment extends Component {
+  render() {
+    const comment = this.props.comment;
+    const mins = Math.floor((Date.now() - comment.get('timestamp')) / 1000 / 60);
+    const time = mins < 1
+            ? 'just now'
+            : mins < 60
+            ? `${mins} min${mins > 1 ? 's' : ''} ago`
+            : `${Math.floor(mins / 60)} hr${Math.floor(mins / 60) > 1 ? 's' : ''} ago`
+
+    return (
+      <div style={{
+        width: 250,
+        textAlign: 'left',
+        borderLeft: `4px solid ${comment.get('optimistic') ? 'gray' : 'green'}`,
+        padding: 10,
+        marginTop: 20,
+      }}>
+        <p style={{padding: 0, margin: 0}}>
+          <span style={{
+             fontSize: 12,
+             marginBottom: 0,
+             paddingBottom: 0,
+             display: 'block',
+          }}>
+            {comment.get('author')} {time}
+          </span>
+          <br/>
+          {comment.get('body')}
+        </p>
+
+      </div>
+    );
+  }
+}
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {projectState};
+  }
+
+  componentDidMount() {
+    this._cleanup = addListener(({newState}) => {
+      this.setState({projectState: newState});
+    });
+  }
+
+  componentWillUnmount() {
+    this._cleanup();
+  }
+
+  render() {
+    const state = this.state.projectState || Map({});
+    const comments = state.get('comments', List());
+    return (
+      <div className="App">
+        <div className="comments">
+          <div>
+            <textarea rows="4" cols="50" onChange={this._onCommentChange} value={this.state.commentBody || ''}/>
+            <div>
+              <button onClick={this._onAddComment}>
+                Add comment
+              </button>
+            </div>
+          </div>
+          {
+            comments.sortBy(x => -x.get('id')).map(comment => <Comment key={comment.get('id')} comment={comment}/>)
+          }
+        </div>
+        <div className="server">
+          <h4>Server Requests</h4>
+          {
+            comments
+              .filter(x => x.get('optimistic'))
+              .sortBy(x => -x.get('id'))
+              .map(comment => <ServerRequest key={comment.get('id')} comment={comment}/>)
+          }
+        </div>
+      </div>
+    );
+  }
+
+  _onCommentChange = (event) => {
+    this.setState({commentBody: event.target.value});
+  }
+
+  _onAddComment = () => {
+    const body = this.state.commentBody;
+    if (!body) {
+      return;
+    }
+
+    updateState(state => {
+      return state.update('comments', List(), (c) => c.push(Map({
+        body: body,
+        author: 'dww',
+        timestamp: Date.now(),
+        id: Date.now(),
+        optimistic: true,
+        commentsBefore: state.get('comments'),
+      })))
+    });
+    this.setState({commentBody: ''});
   }
 }
 
---
+++ src/State.js
@@ -0,0 +1,71 @@
+import Immutable, { Map } from 'immutable';
+
+export var projectState = Map({});
+export var listeners = Map({});
+
+const LOCALSTORAGE_KEY = "_project_state";
+
+function makeKeyGen() {
+  var nextKey = 0;
+
+  return {
+    next: function() {
+      return {value: ":" + (nextKey++).toString(36), done: false};
+    }
+  }
+}
+
+const _genKey = makeKeyGen();
+
+const genKey = () => _genKey.next().value;
+
+function notifyListeners(oldState, newState) {
+  listeners.map((listener, key) => listener({oldState, newState, key}));
+}
+
+function removeListener(key) {
+  listeners = listeners.delete(key);
+}
+
+export function addListener(f, key) {
+  const _key = key ? key : genKey();
+  listeners = listeners.set(_key, f);
+  return removeListener.bind(_key);
+}
+
+export function updateState(f) {
+  const oldState = projectState;
+  const newState = f(oldState);
+  projectState = newState;
+  notifyListeners(oldState, newState);
+  return newState;
+}
+
+function initState() {
+  const stateStr = window.localStorage.getItem(LOCALSTORAGE_KEY);
+  if (stateStr) {
+    const newState = Immutable.fromJS(JSON.parse(stateStr));
+    updateState(() => newState);
+  }
+}
+
+function saveState() {
+  window.localStorage.setItem(
+    LOCALSTORAGE_KEY,
+    JSON.stringify(projectState.toJS()),
+  );
+}
+
+function clearState() {
+  updateState(() => Immutable.Map({}));
+}
+
+window.addEventListener('keydown', (e) => {
+  if (e.ctrlKey && e.key === 'd') {
+    clearState();
+  }
+});
+
+initState();
+
+window.addEventListener('beforeunload', saveState);
